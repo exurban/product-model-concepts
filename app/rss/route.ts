941bd146f02c5ed500d006dac712087993f68ed8
Brad Gibbs <bradley@thegyre.io>
2024-05-06 12:14:07 -0500

    stuff

--- app/rss/route.ts
+++ app/rss/route.ts
@@ -2,23 +2,23 @@ import { baseUrl } from 'app/sitemap'
 import { getLegoEntries } from 'app/lego/utils'
 
 export async function GET() {
-  let allBlogs = await getBlogPosts()
+  let allEntries = await getLegoEntries()
 
-  const itemsXml = allBlogs
+  const itemsXml = allEntries
     .sort((a, b) => {
-      if (new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)) {
+      if (new Date(a.metadata.sortIndex) > new Date(b.metadata.sortIndex)) {
         return -1
       }
       return 1
     })
     .map(
-      (post) =>
+      (entry) =>
         `<item>
-          <title>${post.metadata.title}</title>
-          <link>${baseUrl}/blog/${post.slug}</link>
-          <description>${post.metadata.summary || ''}</description>
+          <title>${entry.metadata.title}</title>
+          <link>${baseUrl}/blog/${entry.slug}</link>
+          <description>${entry.metadata.summary || ''}</description>
           <pubDate>${new Date(
-            post.metadata.publishedAt
+            entry.metadata.publishedAt
           ).toUTCString()}</pubDate>
         </item>`
     )
