ac73d6dc50021186ce57e24613be9f7217dfb9ae
Brad Gibbs <bradley@thegyre.io>
2024-05-07 14:16:03 -0500

    imageResponse

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -29,9 +29,9 @@ export async function GET(request: Request) {
               tw="absolute inset-0 w-full h-full"
               src={image}
             />
-            <div tw="bg-black absolute inset-x-0 inset-y-20 bg-opacity-60"></div>
+            <div tw="bg-black absolute inset-x-0 inset-y-20 bg-opacity-50"></div>
             <div tw="flex flex-col items-start justify-center w-full h-full relative">
-              <div tw="text-[70px] text-rose-700 font-black mx-20">
+              <div tw="text-[70px] text-rose-600 font-black mx-20">
                 Product Model Concepts
               </div>
               <div tw="text-[50px] text-white font-black text-left mx-20">
