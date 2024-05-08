b57dd743fc3a2f2e643962ec4b57c4f7211e49c1
Brad Gibbs <bradley@thegyre.io>
2024-05-08 09:30:19 -0500

    ir

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -14,25 +14,29 @@ export function GET(request: Request) {
 
   return new ImageResponse(
     (
-      <div tw="flex flex-col w-full h-full bg-white">
-        <h1 tw="flex flex-row h-20 text-4xl font-normal border-b-2 border-rose-500 tracking-tight items-center justify-center">
-          product model concepts (lego dub)
-        </h1>
-        <div tw="flex flex-row h-[360px] items-center justify-between bg-yellow-100">
-          <div tw="flex flex-col w-1/2 h-full items-left justify-center text-pretty mx-6 bg-green-500">
-            <h3 tw="text-5xl font-bold">{title}</h3>
-            <p tw="text-4xl font-normal">{description}</p>
+      <>
+        {imageSize === "MD" && (
+          <div tw="flex flex-col w-full h-full bg-white justify-between">
+            <h1 tw="flex flex-row h-20 text-4xl font-normal border-b-2 border-rose-500 tracking-tight items-center justify-center">
+              product model concepts (lego dub)
+            </h1>
+            <div tw="flex flex-row h-[360px] items-center justify-between">
+              <div tw="flex flex-col w-1/2 h-full items-left justify-center text-pretty mx-6">
+                <h3 tw="text-5xl font-bold">{title}</h3>
+                <p tw="text-4xl font-normal">{description}</p>
+              </div>
+              <div tw="flex w-1/3 h-full items-center mx-6">
+                <img style={{ objectFit: "contain" }} tw="" src={image} />
+              </div>
+            </div>
+            <div tw="flex rounded-md shadow justify-end pr-6 pb-6">
+              <a tw="flex items-center justify-center rounded-md border border-transparent bg-rose-500 px-5 py-3 text-base font-medium text-white">
+                Read
+              </a>
+            </div>
           </div>
-          <div tw="flex w-1/3 h-full items-center mx-6 bg-yellow-500">
-            <img style={{ objectFit: "contain" }} tw="" src={image} />
-          </div>
-        </div>
-        <div tw="flex rounded-md shadow justify-end pr-6 pb-6 bg-blue-500">
-          <a tw="flex items-center justify-center rounded-md border border-transparent bg-rose-500 px-5 py-3 text-base font-medium text-white">
-            Read
-          </a>
-        </div>
-      </div>
+        )}
+      </>
     ),
     {
       width: 1200,
