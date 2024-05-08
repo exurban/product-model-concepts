a413978925da95e0a8a9c3c9386cb54d29bbf48d
Brad Gibbs <bradley@thegyre.io>
2024-05-08 08:08:11 -0500

    ir

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -20,12 +20,12 @@ export function GET(request: Request) {
         </h1>
         {imageSize === "MD" ? (
           <>
-            <div tw="flex flex-row w-full items-center justify-between">
-              <div tw="flex flex-col w-1/2 text-pretty mx-6">
-                <h3 tw="text-4xl font-bold">{title}</h3>
-                <p tw="text-3xl font-normal">{description}</p>
+            <div tw="flex flex-row w-full h-2/3 items-center justify-between bg-yellow-100">
+              <div tw="flex flex-col w-1/2 h-full items-left justify-center text-pretty mx-6 bg-green-500">
+                <h3 tw="text-5xl font-bold">{title}</h3>
+                <p tw="text-4xl font-normal">{description}</p>
               </div>
-              <div tw="flex w-1/3 h-full mx-6">
+              <div tw="flex w-1/3 h-full items-center mx-6 bg-yellow-500">
                 <img
                   style={{ objectFit: "contain" }}
                   tw="absolute right-0"
@@ -33,7 +33,7 @@ export function GET(request: Request) {
                 />
               </div>
             </div>
-            <div tw="flex rounded-md shadow self-end m-6">
+            <div tw="flex rounded-md shadow justify-end pr-6 pb-6 bg-blue-500">
               <a tw="flex items-center justify-center rounded-md border border-transparent bg-rose-500 px-5 py-3 text-base font-medium text-white">
                 Read
               </a>
